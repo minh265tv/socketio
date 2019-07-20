c0aece7b7edb6053ba011c62555d8110181c19b6
Minhh <minh.tonn@gmail.com>
2019-07-20 19:52:49 +0700

    modify

--- src/socket/addPatient.js
+++ src/socket/addPatient.js
@@ -4,39 +4,50 @@ const socketCfg = require('../config/socket');
 const config = require('../config/config')
 const fs = require('../../libs/fs');
 
-module.exports = async (token, socket) => {
+const getPatient = async (token) => {
     try {
-
         let authorization = new Authorization();
-
         let patientInfo = await authorization.getPatientInfo(token);
-
-        let patients = fs.find(__dirname, config.DB);
-
-        let check = patients.find(patient => {
-            return patient.patientInfo.id === patientInfo.data.id;
-        });;
-
-        if (!check) {
-            patients.push({
-                patientInfo: patientInfo.data,
-                socketId: [socket.id]
-            });
-
-        } else {
-            check.socketId.push(socket.id);
-
-        }
-
-        fs.write(__dirname, config.DB, patients);
-        console.log(patients)
-        socket.join(socketCfg.room(patientInfo.data.email));
-
-        socket.on('disconnect', () => {
-            disconnect(patientInfo.data, socket);
-            
-        });
+        let { id, name, email } = patientInfo.data;
+        return { id, name, email };
     } catch (error) {
         console.log(error);
     }
+}
+
+const addPatient = (patientInfo, socket) => {
+    let patients = fs.find(__dirname, config.DB);
+
+    let check = patients.find(patient => {
+        return patient.patientInfo.id === patientInfo.id;
+    });;
+
+    if (!check) {
+            patients.push({
+                patientInfo: patientInfo,
+                socketId: [socket.id]
+            });
+    } else {
+        let check1 = check.socketId.find(id => {
+            return id === socket.id
+        });
+        if(!check1)
+            check.socketId.push(socket.id);
+
+    }
+
+    fs.write(__dirname, config.DB, patients);
+    console.log(patients)
+}
+
+
+module.exports = async (token, socket) => {
+    let patientInfo = await getPatient(token);
+    addPatient(patientInfo, socket);
+
+    socket.join(socketCfg.room(patientInfo.email));
+
+    socket.on('disconnect', () => {
+        disconnect(patientInfo, socket);
+    });
 }
--- src/socket/disconnect.js
+++ src/socket/disconnect.js
@@ -3,23 +3,25 @@ const config = require('../config/config')
 const fs = require('../../libs/fs');
 
 module.exports = (patientInfo, socket) => {
-    
+
     let patients = fs.find(__dirname, config.DB);
 
     let patientSocket = patients.find(patient => {
         return patient.patientInfo.id == patientInfo.id;
     });
-    if(!patientSocket) patientSocket = []
-    if (patientSocket.socketId.length > 1) {
-        patientSocket.socketId = patientSocket.socketId.filter((id) => {
-            return id != socket.id;
-        });
-        
-    } else {
-        patients = patients.filter((patient) => {
-            return patient.patientInfo.id != patientSocket.patientInfo.id;
-        });
+    if (patientSocket) {
+        if (patientSocket.socketId.length > 1) {
+            patientSocket.socketId = patientSocket.socketId.filter((id) => {
+                return id != socket.id;
+            });
+
+        } else {
+            patients = patients.filter((patient) => {
+                return patient.patientInfo.id != patientSocket.patientInfo.id;
+            });
+        }
     }
+
     fs.write(__dirname, config.DB, patients);
     console.log(patients)
 }
